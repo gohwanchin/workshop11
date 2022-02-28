fb363775fa76a2bb805c5755dc82ae08252da8fd
Wan Chin <gohwanchin@hotmail.com>
2022-02-28 17:08:08 +0800

    port

--- src/main/java/sg/edu/nus/iss/workshop11/Workshop11Application.java
+++ src/main/java/sg/edu/nus/iss/workshop11/Workshop11Application.java
@@ -1,13 +1,39 @@
 package sg.edu.nus.iss.workshop11;
 
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.Collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class Workshop11Application {
-
+	private static final Logger logger = LoggerFactory.getLogger(Workshop11Application.class);
 	public static void main(String[] args) {
-		SpringApplication.run(Workshop11Application.class, args);
+		logger.info("Workshop 11");
+		logger.debug("Workshop 11 -D");
+		SpringApplication app = new SpringApplication(Workshop11Application.class);
+		String portNum = "3000";
+
+		DefaultApplicationArguments appArgs = new DefaultApplicationArguments(args);
+		List optVals = appArgs.getOptionValues("port");
+		logger.info("optVals > "+ optVals);
+		if (optVals == null || optVals.get(0) == null){
+			portNum = System.getProperty("PORT","3000");
+		}else{
+			portNum = (String)optVals.get(0);
+		}
+
+		if (portNum!=null){
+			app.setDefaultProperties(Collections.singletonMap("server.port",portNum));
+		}
+		//SpringApplication.run(Workshop11Application.class, args);
+		
+		app.run(args);
 	}
 
 }
